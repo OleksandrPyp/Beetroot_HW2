9b85d74991377de4fb221b7c547697a397bb06c1
OleksandrPyp <aleksandrpypenko@gmail.com>
2023-03-27 14:25:34 +0200

    Pushing HW9 (Exceptions)

---
+++ Exceptions/Task 1.py
@@ -0,0 +1,12 @@
+#Task 1
+def oops():
+    raise IndexError
+def solution_oops():
+    try:
+        oops()
+    except IndexError:
+        print("It was an error but we solved it")
+
+print(solution_oops())
+
+#if we substitute the IndexError with KeyError function is not going to work properly and going to return the KeyError
---
+++ Exceptions/Task 2.py
@@ -0,0 +1,16 @@
+#Task 2
+def square_func():
+    a = int(input("Please enter the number you want to have squared: "))
+    b = int(input("Please enter the number you use as a divider: "))
+    c = a ** 2 / b
+    print(c)
+
+while True:
+    try:
+        square_func()
+    except ZeroDivisionError:
+        print("Division by zero!")
+    except ValueError:
+        print("Please input the number! ")
+
+print(square_func())
